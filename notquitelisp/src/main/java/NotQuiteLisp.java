1da47289ab0d943404f8da1c4008f0e8f04854d4
Blaise Wielk <bwielk@gmail.com>
2022-11-02 21:20:43 +0000

    refactor

--- notquitelisp/src/main/java/NotQuiteLisp.java
+++ notquitelisp/src/main/java/NotQuiteLisp.java
@@ -13,13 +13,7 @@ public class NotQuiteLisp {
 
 		int level = 0;
 
-		List<String> content = FileReaderHelper.readFileAsLinesOfStrings( NotQuiteLisp.class.getClassLoader(),
-				schemaName );
-
-		if( content.size() != 1 ){
-			throw new IllegalArgumentException(ErrorContent.INVALID_ROW_NUMBER);
-		}
-
+		List<String> content = readFile(schemaName);
 		String input = content.get( 0 );
 
 		for(int i=0; i<input.length(); i++){
@@ -33,18 +27,19 @@ public class NotQuiteLisp {
 		return level;
 	}
 
+	/***
+	 *
+	 * @param schemaName - name of the file e.g. "input.txt"
+	 * @return records about how many times, when and at what level the basement was reached
+	 */
+
 	public List<BasementRecord> investigateReachingBasement( String schemaName ) {
 
 		int level = 0;
+
 		List<BasementRecord> basementRecords = new ArrayList<>();
 
-		List<String> content = FileReaderHelper.readFileAsLinesOfStrings( NotQuiteLisp.class.getClassLoader(),
-				schemaName );
-
-		if( content.size() != 1 ){
-			throw new IllegalArgumentException(ErrorContent.INVALID_ROW_NUMBER);
-		}
-
+		List<String> content = readFile(schemaName);
 		String input = content.get( 0 );
 
 		for(int i=0; i<input.length(); i++){
@@ -60,5 +55,16 @@ public class NotQuiteLisp {
 		}
 
 		return basementRecords;
+	}
+
+	private List<String> readFile(String schemaName){
+		List<String> content = FileReaderHelper.readFileAsLinesOfStrings( NotQuiteLisp.class.getClassLoader(),
+				schemaName );
+
+		if( content.size() != 1 ){
+			throw new IllegalArgumentException(ErrorContent.INVALID_ROW_NUMBER);
+		}
+
+		return content;
 	}
 }
