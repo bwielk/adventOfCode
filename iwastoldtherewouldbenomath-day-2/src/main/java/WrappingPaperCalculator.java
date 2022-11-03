95a5979abd89167bd35e56c40021c8640f49085c
Blaise Wielk <bwielk@gmail.com>
2022-11-03 14:52:17 +0000

    managed to calculate the task 1 of day 2 but there are still more tests to be written

--- iwastoldtherewouldbenomath-day-2/src/main/java/WrappingPaperCalculator.java
+++ iwastoldtherewouldbenomath-day-2/src/main/java/WrappingPaperCalculator.java
@@ -8,16 +8,51 @@ public class WrappingPaperCalculator {
 
 		int result = 0;
 
-		List<String> contents =  FileReaderHelper.readFileAsLinesOfStrings( WrappingPaperCalculator.class, inputSchema );
+		List<String> contents = FileReaderHelper.readFileAsLinesOfStrings(
+				WrappingPaperCalculator.class, inputSchema );
 
-		for(int i=0; i<contents.size(); i++){
-			List<Integer> measurements = Arrays.stream(contents.get( i ).split( "x" ))
-					.map( Integer::parseInt ).collect( Collectors.toList());
-
-			result += (3*measurements.get( 0 )*measurements.get( 1 )) +
-					(2*measurements.get( 1 )*measurements.get( 2 )) +
-					(2*measurements.get( 0 )*measurements.get( 2 ));
+		for ( int i = 0; i < contents.size(); i++ ) {
+			String currentLine = contents.get( i );
+			result += parseEntry( currentLine );
 		}
 		return result;
+	}
+
+	public int parseEntry( String entry ) {
+		int result = 0;
+		String cleanedEntry = entry.replace( " ", "" )
+				.trim()
+				.toLowerCase()
+				.replaceAll( "/^v[1-9x]/g", "" );
+
+		List<String> separated = Arrays.stream( cleanedEntry.split( "x" ) )
+				.filter( x -> !x.equals( "" ) )
+				.collect( Collectors.toList() );
+
+
+		List<Integer> measurements = separated.stream()
+				.map( Integer::parseInt )
+				.collect( Collectors.toList() );
+		if ( measurements.size() == 3 && !measurements.contains( 0 ) ) {
+			int sideA = measurements.get( 0 ) * measurements.get( 1 );
+			int sideB = measurements.get( 1 ) * measurements.get( 2 );
+			int sideC = measurements.get( 0 ) * measurements.get( 2 );
+
+			List<Integer> collectionOfAreaValues = Arrays.asList( sideA, sideB, sideC );
+			int slack = collectionOfAreaValues.get( 0 );
+			for(int i=0; i<collectionOfAreaValues.size(); i++){
+				if(collectionOfAreaValues.get( i ) < slack){
+					slack = collectionOfAreaValues.get( i );
+				}
+			}
+			result += 2*(sideA+sideB+sideC)+slack;
+		}
+		return result;
+	}
+
+	public static void main( String[] args ) {
+		WrappingPaperCalculator wrappingPaperCalculator = new WrappingPaperCalculator();
+		int results = wrappingPaperCalculator.calculateFromFile( "input.txt" );
+		System.out.println(results);
 	}
 }
--- iwastoldtherewouldbenomath-day-2/src/test/java/WrappingPaperCalculatorTest.java
+++ iwastoldtherewouldbenomath-day-2/src/test/java/WrappingPaperCalculatorTest.java
@@ -27,11 +27,100 @@ class WrappingPaperCalculatorTest {
 	}
 
 	@Test
+	public void canCalculateBasedOnMixtureOfCorrectlyAndIncorrectlyFormattedMeasurements(){
+		int result = wrappingPaperCalculator.calculateFromFile("wrongInputs.txt");
+		int expectedResult = 958;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
 	public void canCalculateBasicMeasurementTwoByThreeByFourAndOneByOneByTen(){
 		int result = wrappingPaperCalculator.calculateFromFile("bigBoxesInput.txt");
 		int expectedResult = 101;
 		assertThat(result).isEqualTo( expectedResult );
 	}
 
+	@Test
+	public void canIgnoreNonRectangularInputsFourMeasurementsInsteadOfThree(){
+		int result = wrappingPaperCalculator.parseEntry("1x1x1x1");
+		int expectedResult = 0;
+		assertThat(result).isEqualTo( expectedResult );
+	}
 
+	@Test
+	public void canIgnoreNonRectangularInputsTwoMeasurementsInsteadOfThree(){
+		int result = wrappingPaperCalculator.parseEntry("1x1");
+		int expectedResult = 0;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void canParseEntryAsLongAsTheEntryContainsThreeSeparateDigits(){
+		int result = wrappingPaperCalculator.parseEntry("1x1x1x");
+		int expectedResult = 7;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void xDelimeterCanBeAsCapitalOrNonCapitalCharacter(){
+		int result = wrappingPaperCalculator.parseEntry("1x1X1");
+		int expectedResult = 7;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void canExcludeEntriesWithZerosInIt(){
+		int result = wrappingPaperCalculator.parseEntry("1x1x0");
+		int expectedResult = 0;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void canHandleEmptyEntries(){
+		int result = wrappingPaperCalculator.parseEntry("");
+		int expectedResult = 0;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void canHandleTensTeensAndHundreds(){
+		int result = wrappingPaperCalculator.parseEntry("11x30x200");
+		int expectedResult = 17390;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void onlyXDelimetersAreAcceptedOthersBreakAnEntry(){
+		int result = wrappingPaperCalculator.parseEntry("1x1z1");
+		int expectedResult = 0;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void duplicatedXDelimetersAreNotAccepted(){
+		int result = wrappingPaperCalculator.parseEntry("1xx1xx1");
+		int expectedResult = 7;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void spacesBetweenDelimetersAreIgnored(){
+		int result = wrappingPaperCalculator.parseEntry("1 x 1 x 1");
+		int expectedResult = 7;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void leadingZerosAreIgnoredAndResultInProcessedEntry(){
+		int result = wrappingPaperCalculator.parseEntry("01x01x1");
+		int expectedResult = 7;
+		assertThat(result).isEqualTo( expectedResult );
+	}
+
+	@Test
+	public void additionalIncorrectDelimeterIsIgnored(){
+		int result = wrappingPaperCalculator.parseEntry("2x3x4xz");
+		int expectedResult = 58;
+		assertThat(result).isEqualTo( expectedResult );
+	}
 }
