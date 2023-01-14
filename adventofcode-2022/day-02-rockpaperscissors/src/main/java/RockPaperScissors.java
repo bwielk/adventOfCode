a04ccdc0f4c61a60d6534bf788223a867563701a
Blaise Wielk <bwielk@gmail.com>
2023-01-14 16:57:11 +0000

    day 2 2022 - able to turn entries into moves

--- adventofcode-2022/day-02-rockpaperscissors/src/main/java/RockPaperScissors.java
+++ adventofcode-2022/day-02-rockpaperscissors/src/main/java/RockPaperScissors.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.NonNull;
 
 public class RockPaperScissors {
 
@@ -9,40 +13,72 @@ public class RockPaperScissors {
 		return gameRound;
 	}
 
-	public List<RPSResult> resolveRound( final RPSMoves playerOneMove,
-			final RPSMoves playerTwoMove ) {
+	public List<RPSMoves> translateEntries( String entry ) {
+		List<RPSMoves> moves = new ArrayList<>();
+		List<String> cleansedEntry = Arrays.asList(
+				entry.strip().replaceAll( " ", "" ).split( "" ) );
+		if ( cleansedEntry.size() == 2 && cleansedEntry.get( 0 ).length() == 1 && cleansedEntry.get(
+				1 ).length() == 1 ) {
+			List<Character> player1AllowedParams = Arrays.stream( RPSMoves.values() )
+					.map( x -> Character.toUpperCase( x.getSchema().get( 0 ) ) )
+					.collect( Collectors.toList() );
+			List<Character> player2AllowedParams = Arrays.stream( RPSMoves.values() )
+					.map( x -> Character.toUpperCase( x.getSchema().get( 1 ) ) )
+					.collect( Collectors.toList() );
+			char entryPlayer1 = Character.toUpperCase(cleansedEntry.get( 0 ).charAt( 0 ) );
+			char entryPlayer2 = Character.toUpperCase(cleansedEntry.get( 1 ).charAt( 0 ) );
+			if ( player1AllowedParams.contains( entryPlayer1 ) &&
+					player2AllowedParams.contains( entryPlayer2 )){
+				for(RPSMoves move : RPSMoves.values()){
+					if(move.getSchema().get( 0 ).equals( entryPlayer1 )){
+						moves.add( move );
+					}
+					if(move.getSchema().get( 1 ).equals( entryPlayer2 )){
+						moves.add( move);
+					}
+				}
+			}else{
+				throw new IllegalStateException( String.format("The entry '%s' should only contain acceptable content", entry ));
+			}
+		} else {
+			throw new IllegalStateException( "The entry should only contain two parameters" );
+		} return moves;
+	}
+
+	public List<RPSResult> resolveRound( @NonNull final RPSMoves playerOneMove,
+			@NonNull final RPSMoves playerTwoMove ) {
 		List<RPSResult> results = new ArrayList<>();
 		gameRound.add( playerOneMove );
 		gameRound.add( playerTwoMove );
 		if ( gameRound.contains( RPSMoves.PAPER ) && gameRound.contains( RPSMoves.ROCK ) ) {
-			if(gameRound.get( 0 ) == RPSMoves.PAPER){
-				results.add(RPSResult.WIN );
-				results.add(RPSResult.LOST );
-			}else{
-				results.add(RPSResult.LOST );
-				results.add(RPSResult.WIN );
+			if ( gameRound.get( 0 ) == RPSMoves.PAPER ) {
+				results.add( RPSResult.WIN );
+				results.add( RPSResult.LOST );
+			} else {
+				results.add( RPSResult.LOST );
+				results.add( RPSResult.WIN );
 			}
 		} else if ( gameRound.contains( RPSMoves.ROCK ) && gameRound.contains(
 				RPSMoves.SCISSORS ) ) {
-			if(gameRound.get( 0 ) == RPSMoves.ROCK){
-				results.add(RPSResult.WIN );
-				results.add(RPSResult.LOST );
-			}else{
-				results.add(RPSResult.LOST );
-				results.add(RPSResult.WIN );
+			if ( gameRound.get( 0 ) == RPSMoves.ROCK ) {
+				results.add( RPSResult.WIN );
+				results.add( RPSResult.LOST );
+			} else {
+				results.add( RPSResult.LOST );
+				results.add( RPSResult.WIN );
 			}
 		} else if ( gameRound.contains( RPSMoves.SCISSORS ) && gameRound.contains(
 				RPSMoves.PAPER ) ) {
-			if(gameRound.get( 0 ) == RPSMoves.SCISSORS){
-				results.add(RPSResult.WIN );
-				results.add(RPSResult.LOST );
-			}else{
-				results.add(RPSResult.LOST );
-				results.add(RPSResult.WIN );
+			if ( gameRound.get( 0 ) == RPSMoves.SCISSORS ) {
+				results.add( RPSResult.WIN );
+				results.add( RPSResult.LOST );
+			} else {
+				results.add( RPSResult.LOST );
+				results.add( RPSResult.WIN );
 			}
 		} else if ( gameRound.get( 0 ) == gameRound.get( 1 ) ) {
-			results.add(RPSResult.DRAW );
-			results.add(RPSResult.DRAW );
+			results.add( RPSResult.DRAW );
+			results.add( RPSResult.DRAW );
 		}
 		gameRound.clear();
 		return results;
--- adventofcode-2022/day-02-rockpaperscissors/src/test/java/RockPaperScissorsTest.java
+++ adventofcode-2022/day-02-rockpaperscissors/src/test/java/RockPaperScissorsTest.java
@@ -1,5 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -38,6 +40,32 @@ class RockPaperScissorsTest {
 				Arguments.of(RPSMoves.SCISSORS, RPSMoves.PAPER, RPSResult.WIN, RPSResult.LOST)
 		);
 	}
+
+	private static Stream<Arguments> correctlyFormattedEntries() {
+		return Stream.of(
+				Arguments.of("C Z", RPSMoves.SCISSORS, RPSMoves.SCISSORS),
+				Arguments.of("C Y", RPSMoves.SCISSORS, RPSMoves.PAPER),
+				Arguments.of("c y", RPSMoves.SCISSORS, RPSMoves.PAPER),
+				Arguments.of("   A Z   ", RPSMoves.ROCK, RPSMoves.SCISSORS),
+//				Arguments.of("", Collections.emptyList() ),
+//				Arguments.of("   ", Collections.emptyList()),
+				Arguments.of("AZ", RPSMoves.ROCK, RPSMoves.SCISSORS)
+		);
+	}
+
+	private static Stream<Arguments> incorrectlyFormattedEntries() {
+		return Stream.of(
+				Arguments.of("Z C"),
+				Arguments.of("Z C C"),
+				Arguments.of("Z,C"),
+				Arguments.of("1 C"),
+				Arguments.of("chess zest"),
+				Arguments.of("hello")
+				);
+	}
+
+
+
 	@ParameterizedTest
 	@MethodSource("paperAndRock")
 	public void paperWinsOverRock(RPSMoves move1, RPSMoves move2, RPSResult result1, RPSResult result2){
@@ -66,8 +94,31 @@ class RockPaperScissorsTest {
 	}
 
 	@Test
+	public void drawsAreDetected(){
+		List<RPSResult> resultList = rockPaperScissors.resolveRound( RPSMoves.SCISSORS, RPSMoves.SCISSORS );
+		assertThat(resultList).hasSize( 2 );
+		assertThat( resultList.get( 0 )).isEqualTo( RPSResult.DRAW );
+		assertThat( resultList.get( 1 )).isEqualTo( RPSResult.DRAW );
+	}
+
+	@Test
+	public void eitherOfArgumentsCannotBeNull(){
+		assertThrows(NullPointerException.class,
+				() -> rockPaperScissors.resolveRound( null, RPSMoves.SCISSORS ));
+	}
+
+
+	@Test
 	public void eachRoundGetsCleared(){
 		rockPaperScissors.resolveRound( RPSMoves.SCISSORS, RPSMoves.PAPER );
 		assertThat( rockPaperScissors.getGameRound()).isEmpty();
+	}
+
+	@ParameterizedTest
+	@MethodSource("correctlyFormattedEntries")
+	public void entriesCanBeTranslatedToGameMoves(String entry, RPSMoves move1, RPSMoves move2){
+		List<RPSMoves> moves = rockPaperScissors.translateEntries( entry );
+		assertThat( moves ).hasSize( 2 );
+		assertThat( moves ).containsExactlyInAnyOrder( move1, move2 );
 	}
 }
