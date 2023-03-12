1edd78808d8dceec80d8f250d9cd8f61f832c6f7
Blaise Wielk <bwielk@gmail.com>
2023-03-12 10:00:18 +0000

    refactored task day 1 - refactored the code further

--- adventofcode-2022/day-01-caloriecounting/src/main/java/CalorieCounting.java
+++ adventofcode-2022/day-01-caloriecounting/src/main/java/CalorieCounting.java
@@ -5,44 +5,49 @@ import java.util.List;
 public class CalorieCounting {
 
 	public Integer runFindHighestCalorieIntake( String fileName ) {
-		List<Integer> intake = createListOfElfCalorieIntake( fileName );
+		List<String> inputs = FileReaderHelper.readFileAsLinesOfStrings( CalorieCounting.class,
+				fileName );
+		List<Integer> intake = createListOfElfCalorieIntake( inputs );
 		return findHighestCalorieIntake( intake );
 	}
 
 	public Integer runFindHighestCalorieIntakeForTopThreeElves( String fileName ) {
-		List<Integer> intake = createListOfElfCalorieIntake( fileName );
+		List<String> inputs = FileReaderHelper.readFileAsLinesOfStrings( CalorieCounting.class,
+				fileName );
+		List<Integer> intake = createListOfElfCalorieIntake( inputs );
 		return findHighestCalorieIntakeForTopThreeElves( intake );
 	}
 
-	public Integer findHighestCalorieIntakeForTopThreeElves( List<Integer> intake){
+	public Integer findHighestCalorieIntakeForTopThreeElves( List<Integer> intake ) {
 		Collections.sort( intake );
 		List<Integer> highestIntake;
-		if( intake.size() >= 3 ){
+		if ( intake.size() >= 3 ) {
 			highestIntake = intake.subList( 0, 3 );
-		}else{
+		} else {
 			highestIntake = intake.subList( 0, intake.size() );
 		}
-		return highestIntake.stream().mapToInt(Integer::intValue).sum();
+		return highestIntake.stream().mapToInt( Integer::intValue ).sum();
 	}
 
-	public Integer findHighestCalorieIntake( List<Integer> intake){
+	public Integer findHighestCalorieIntake( List<Integer> intake ) {
 		Collections.sort( intake );
 		return intake.get( intake.size() - 1 );
 	}
 
-	public List<Integer> createListOfElfCalorieIntake( String fileName ) {
-		List<String> inputs = FileReaderHelper.readFileAsLinesOfStrings( CalorieCounting.class,
-				fileName );
+	public List<Integer> createListOfElfCalorieIntake( List<String> inputs ) {
 		List<Integer> records = new ArrayList<>();
 		int caloriesSumRecorder = 0;
-		for ( int i = 0; i<inputs.size(); i++) {
-			String clearedLine = inputs.get( i ).strip();
-			if ( clearedLine.isEmpty() && ( i<inputs.size() && !inputs.get( i+1 ).isEmpty() )) {
+		for ( int i = 0; i < inputs.size(); i++ ) {
+			String sanitisedLine = inputs.get( i ).replace( " ", "" ).strip();
+			if ( sanitisedLine.isEmpty()
+					&& ( i + 1 < inputs.size()
+					&& !inputs.get( i + 1 ).isEmpty() )
+					&& caloriesSumRecorder != 0 ) {
 				records.add( caloriesSumRecorder );
 				caloriesSumRecorder = 0;
 			} else {
-				if(!clearedLine.isEmpty()){
-					caloriesSumRecorder += Integer.parseInt( clearedLine );
+				if ( !sanitisedLine.isEmpty() ) {
+					caloriesSumRecorder += Integer.parseInt( sanitisedLine );
 				}
 			}
 		}
--- adventofcode-2022/day-01-caloriecounting/src/test/java/CalorieCountingTest.java
+++ adventofcode-2022/day-01-caloriecounting/src/test/java/CalorieCountingTest.java
@@ -10,29 +10,58 @@ class CalorieCountingTest {
 	private CalorieCounting calorieCounting;
 
 	@BeforeEach
-	public void before(){
+	public void before() {
 		calorieCounting = new CalorieCounting();
 	}
 
 	@Test
-	public void inputCanBeTranslatedIntoAListOfSumsOfIntakes(){
-		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake("testInput.txt");
-		assertThat(sums.size()).isEqualTo( 4 );
+	public void inputCannotBeTranslatedIntoAListOfSumsOfIntakesIfFileEmpty() {
+		List<String> inputs = List.of( "" );
+		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake(inputs);
+		assertThat( sums.size() ).isEqualTo( 0 );
+	}
+
+	@Test
+	public void inputCanBeTranslatedIntoAListOfSumsOfIntakesStandardFormatWithSingularEntry() {
+		List<String> inputs = List.of( "1" );
+		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake(inputs);
+		assertThat( sums.size() ).isEqualTo( 1 );
+		assertThat( sums ).containsExactlyInAnyOrder( 1 );
+	}
+
+	@Test
+	public void inputCanBeTranslatedIntoAListOfSumsOfIntakesStandardFormatWithSingularSpacing() {
+		List<String> inputs = List.of( "1", "", "2", "3", "", "4", "5", "6", "", "7", "8" );
+		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake(inputs);
+		assertThat( sums.size() ).isEqualTo( 4 );
 		assertThat( sums ).containsExactlyInAnyOrder( 1, 5, 15, 15 );
 	}
 
 	@Test
-	public void canFindHighestIntakeBasedOnRealInput(){
+	public void inputCanBeTranslatedIntoAListOfSumsOfIntakesStandardFormatWithMultipleSpacing() {
+		List<String> inputs = List.of( "1", "", "2", "3", "", "", "4", "5", "6", "", "", "", "7", "8" );
+		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake(inputs);
+		assertThat( sums.size() ).isEqualTo( 4 );
+		assertThat( sums ).containsExactlyInAnyOrder( 1, 5, 15, 15 );
+	}
+
+	@Test
+	public void inputCanBeTranslatedIntoAListOfSumsOfIntakesStandardFormatWithMultipleLeadingAndTrailingSpacing() {
+		List<String> inputs = List.of(" ", "1", "", "2", "3", "", "", "4", "5", "6", "", "", "", "7", "8", " " );
+		List<Integer> sums = calorieCounting.createListOfElfCalorieIntake(inputs);
+		assertThat( sums.size() ).isEqualTo( 4 );
+		assertThat( sums ).containsExactlyInAnyOrder( 1, 5, 15, 15 );
+	}
+
+	@Test
+	public void canFindHighestIntakeBasedOnRealInput() {
 		int result = calorieCounting.runFindHighestCalorieIntake( "input.txt" );
 		assertThat( result ).isEqualTo( 67633 );
 	}
 
 	@Test
-	public void canFindSumOfThreeHighestIntakesBasedOnRealInput(){
+	public void canFindSumOfThreeHighestIntakesBasedOnRealInput() {
 		int result = calorieCounting.runFindHighestCalorieIntakeForTopThreeElves( "input.txt" );
 		assertThat( result ).isEqualTo( 199628 );
 	}
-		//findHighestCalorieIntakeForTopThreeElves
-		//findHighestCalorieIntake
-		//createListOfElfCalorieIntake
 }
