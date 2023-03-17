77d995b7b88455d96dc347095dbaa88a02ad70bd
Blaise Wielk <bwielk@gmail.com>
2023-03-17 19:00:00 +0000

    simple solution implemented

--- adventofcode-2022/day-04-campcleanup/src/main/java/CampCleanup.java
+++ adventofcode-2022/day-04-campcleanup/src/main/java/CampCleanup.java
@@ -1,1 +1,9 @@
-public class CampCleanup {}
+import java.util.List;
+
+public class CampCleanup {
+
+	public boolean findIfPairsAreMutuallyContained( final List<Integer> input ) {
+		return (input.get( 0 ) >= input.get( 2 ) && input.get( 1 ) <= input.get( 3 )) ||
+				(input.get( 0 ) <= input.get( 2 ) && input.get( 1 ) >= input.get( 3 ));
+	}
+}
--- adventofcode-2022/day-04-campcleanup/src/test/java/CampCleanupTest.java
+++ adventofcode-2022/day-04-campcleanup/src/test/java/CampCleanupTest.java
@@ -1,5 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class CampCleanupTest {
+
+	private CampCleanup campCleanup;
+
+	@BeforeEach
+	public void before(){
+		campCleanup = new CampCleanup();
+	}
+
+	@Test
+	public void firstPairIsNotContainedWithinTheSecondOne(){
+		List<Integer> input = List.of(1,2,2,3);
+		boolean result = campCleanup.findIfPairsAreMutuallyContained(input);
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	public void secondPairIsNotContainedWithinTheFirstOne() {
+		List<Integer> input = List.of( 2, 4, 3, 5 );
+		boolean result = campCleanup.findIfPairsAreMutuallyContained( input );
+		assertThat( result ).isFalse();
+	}
+
+	@Test
+	public void firstPairIsContainedWithinTheSecondOne(){
+		List<Integer> input = List.of(2,4,1,5);
+		boolean result = campCleanup.findIfPairsAreMutuallyContained(input);
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	public void secondPairIsContainedWithinTheFirstOne(){
+		List<Integer> input = List.of(1,6,4,6);
+		boolean result = campCleanup.findIfPairsAreMutuallyContained(input);
+		assertThat(result).isTrue();
+	}
 
 }
