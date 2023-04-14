0bf27c8cfbe9fb74b945861a95c54d746506630b
Blaise Wielk <bwielk@gmail.com>
2023-04-14 15:26:51 +0100

    expanded unit tests for matrix parsing. Proceeding to commands parsing

--- adventofcode-2022/day-05-supplystacks/src/main/java/SupplyStacks.java
+++ adventofcode-2022/day-05-supplystacks/src/main/java/SupplyStacks.java
@@ -4,26 +4,37 @@ import java.util.Stack;
 
 public class SupplyStacks {
 
-	public List<Stack<Character>> generateStacksFromContent(String nameOfFile){
-		List<String> readLines = FileReaderHelper.readFileAsLinesOfStrings( SupplyStacks.class, nameOfFile );
+	private final List<Stack<Character>> stacks = new ArrayList<>();
+
+	public List<Stack<Character>> generateStacksFromContent( String nameOfFile ) {
+		List<String> readLines = FileReaderHelper.readFileAsLinesOfStrings( SupplyStacks.class,
+				nameOfFile );
+		return extractStacks( readLines );
+	}
+
+	public List<Stack<Character>> extractStacks( List<String> readLines ) {
 		int beginningOfStackMap = -1;
-		for(String l : readLines){
-			if(l.matches( UsefulRegex.ONLY_NUMBERS_WITH_SPACES ) && !l.isEmpty()){
+		for ( String l : readLines ) {
+			if ( l.matches( UsefulRegex.ONLY_NUMBERS_WITH_SPACES ) && !l.isEmpty() ) {
 				beginningOfStackMap = readLines.indexOf( l );
 				break;
 			}
 		}
-		List<Stack<Character>> stacks = new ArrayList<>();
-		for(int n=0; n<readLines.get( beginningOfStackMap ).length(); n++){
-				if(Character.isDigit(readLines.get( beginningOfStackMap ).charAt( n ))){
-					Stack<Character> stack = new Stack<>();
-					for(int i= beginningOfStackMap-1; i> -1; i--){
-						String currentLine = readLines.get( i );
-						if(currentLine.length() >= n && currentLine.charAt( n )!= ' '){
-							stack.push( currentLine.charAt( n ));
-						}
+
+		if ( beginningOfStackMap < 0 ) {
+			return stacks;
+		}
+
+		for ( int n = 0; n < readLines.get( beginningOfStackMap ).length(); n++ ) {
+			if ( Character.isDigit( readLines.get( beginningOfStackMap ).charAt( n ) ) ) {
+				Stack<Character> stack = new Stack<>();
+				for ( int i = beginningOfStackMap - 1; i > -1; i-- ) {
+					String currentLine = readLines.get( i );
+					if ( currentLine.length() >= n && currentLine.charAt( n ) != ' ' ) {
+						stack.push( currentLine.charAt( n ) );
 					}
-					stacks.add( stack );
+				}
+				stacks.add( stack );
 			}
 		}
 		return stacks;
--- adventofcode-2022/day-05-supplystacks/src/test/java/SupplyStacksTest.java
+++ adventofcode-2022/day-05-supplystacks/src/test/java/SupplyStacksTest.java
@@ -1,5 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.List;
 import java.util.Stack;
 
@@ -11,12 +11,12 @@ class SupplyStacksTest {
 	private SupplyStacks supplyStacks;
 
 	@BeforeEach
-	public void before(){
+	public void before() {
 		supplyStacks = new SupplyStacks();
 	}
 
 	@Test
-	public void extractStacks(){
+	public void extractStacks_happyPathWithMixtureOfStackHeights() {
 		Stack<Character> stack1 = new Stack<>();
 		stack1.push( 'S' );
 		stack1.push( 'Q' );
@@ -43,15 +43,146 @@ class SupplyStacksTest {
 		stack6.push( 'U' );
 
 		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent( "test.txt" );
-		assertThat(actualStacks).hasSize( 6 );
+		assertThat( actualStacks ).hasSize( 6 );
 
-		assertThat( actualStacks.get( 0 )).containsExactlyElementsOf( stack1 );
-		assertThat( actualStacks.get( 1 )).containsExactlyElementsOf( stack2 );
-		assertThat( actualStacks.get( 2 )).containsExactlyElementsOf( stack3 );
-		assertThat( actualStacks.get( 3 )).containsExactlyElementsOf( stack4 );
-		assertThat( actualStacks.get( 4 )).containsExactlyElementsOf( stack5 );
-		assertThat( actualStacks.get( 5 )).containsExactlyElementsOf( stack6 );
-
+		assertThat( actualStacks.get( 0 ) ).containsExactlyElementsOf( stack1 );
+		assertThat( actualStacks.get( 1 ) ).containsExactlyElementsOf( stack2 );
+		assertThat( actualStacks.get( 2 ) ).containsExactlyElementsOf( stack3 );
+		assertThat( actualStacks.get( 3 ) ).containsExactlyElementsOf( stack4 );
+		assertThat( actualStacks.get( 4 ) ).containsExactlyElementsOf( stack5 );
+		assertThat( actualStacks.get( 5 ) ).containsExactlyElementsOf( stack6 );
 	}
 
+	@Test
+	public void extractStacks_emptyValuesInCratesAreIgnored() {
+		Stack<Character> stack1 = new Stack<>();
+		stack1.push( 'S' );
+		stack1.push( 'S' );
+		stack1.push( 'Q' );
+
+		Stack<Character> stack2 = new Stack<>();
+		stack2.push( 'R' );
+		stack2.push( 'R' );
+
+		Stack<Character> stack3 = new Stack<>();
+		stack3.push( 'B' );
+
+		Stack<Character> stack4 = new Stack<>();
+		stack4.push( 'V' );
+
+		Stack<Character> stack5 = new Stack<>();
+
+		Stack<Character> stack6 = new Stack<>();
+		stack6.push( 'B' );
+		stack6.push( 'S' );
+
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent( "stackscontainemptyvaluesinbetweencrates.txt" );
+		assertThat( actualStacks ).hasSize( 6 );
+
+		assertThat( actualStacks.get( 0 ) ).containsExactlyElementsOf( stack1 );
+		assertThat( actualStacks.get( 1 ) ).containsExactlyElementsOf( stack2 );
+		assertThat( actualStacks.get( 2 ) ).containsExactlyElementsOf( stack3 );
+		assertThat( actualStacks.get( 3 ) ).containsExactlyElementsOf( stack4 );
+		assertThat( actualStacks.get( 4 ) ).containsExactlyElementsOf( stack5 );
+		assertThat( actualStacks.get( 5 ) ).containsExactlyElementsOf( stack6 );
+	}
+
+
+	@Test
+	public void extractStacks_stacksCanContainAnyCharacterType() {
+		Stack<Character> stack1 = new Stack<>();
+		stack1.push( 'S' );
+		stack1.push( 'Q' );
+		stack1.push( 'S' );
+		stack1.push( '0' );
+
+		Stack<Character> stack2 = new Stack<>();
+		stack2.push( 'R' );
+		stack2.push( 'Q' );
+		stack2.push( '1' );
+
+		Stack<Character> stack3 = new Stack<>();
+		stack3.push( 'Z' );
+		stack3.push( ',' );
+
+		Stack<Character> stack4 = new Stack<>();
+		stack4.push( ']' );
+
+		Stack<Character> stack5 = new Stack<>();
+
+		Stack<Character> stack6 = new Stack<>();
+		stack6.push( '8' );
+		stack6.push( 'S' );
+		stack6.push( 'a' );
+
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent( "stackscontainingvariouschartypes.txt" );
+		assertThat( actualStacks ).hasSize( 6 );
+
+		assertThat( actualStacks.get( 0 ) ).containsExactlyElementsOf( stack1 );
+		assertThat( actualStacks.get( 1 ) ).containsExactlyElementsOf( stack2 );
+		assertThat( actualStacks.get( 2 ) ).containsExactlyElementsOf( stack3 );
+		assertThat( actualStacks.get( 3 ) ).containsExactlyElementsOf( stack4 );
+		assertThat( actualStacks.get( 4 ) ).containsExactlyElementsOf( stack5 );
+		assertThat( actualStacks.get( 5 ) ).containsExactlyElementsOf( stack6 );
+	}
+
+	@Test
+	public void extractStacks_stackNominatorCanContainDuplicatedSingleDigitNumbers() {
+		Stack<Character> stack1 = new Stack<>();
+		stack1.push( 'S' );
+		stack1.push( 'Q' );
+		stack1.push( 'S' );
+		stack1.push( 'Q' );
+
+		Stack<Character> stack2 = new Stack<>();
+		stack2.push( 'R' );
+		stack2.push( 'Q' );
+		stack2.push( 'R' );
+
+		Stack<Character> stack3 = new Stack<>();
+		stack3.push( 'Z' );
+		stack3.push( 'B' );
+
+		Stack<Character> stack4 = new Stack<>();
+		stack4.push( 'V' );
+
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent( "columnnominatorcontainsrandomsingledigitnumbers.txt" );
+		assertThat( actualStacks ).hasSize( 4 );
+
+		assertThat( actualStacks.get( 0 ) ).containsExactlyElementsOf( stack1 );
+		assertThat( actualStacks.get( 1 ) ).containsExactlyElementsOf( stack2 );
+		assertThat( actualStacks.get( 2 ) ).containsExactlyElementsOf( stack3 );
+		assertThat( actualStacks.get( 3 ) ).containsExactlyElementsOf( stack4 );
+	}
+
+	@Test
+	public void extractStacks_fileWithoutDefinedColumns() {
+		Stack<Character> stack1 = new Stack<>();
+		Stack<Character> stack2 = new Stack<>();
+		Stack<Character> stack3 = new Stack<>();
+		Stack<Character> stack4 = new Stack<>();
+
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent(
+				"nocolumns.txt" );
+		assertThat( actualStacks ).hasSize( 6 );
+
+		assertThat( actualStacks.get( 0 ) ).containsExactlyElementsOf( stack1 );
+		assertThat( actualStacks.get( 1 ) ).containsExactlyElementsOf( stack2 );
+		assertThat( actualStacks.get( 2 ) ).containsExactlyElementsOf( stack3 );
+		assertThat( actualStacks.get( 3 ) ).containsExactlyElementsOf( stack4 );
+	}
+
+	@Test
+	public void extractStacks_missingColumnsNominator() {
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent(
+				"missingcolumnnominator.txt" );
+		assertThat( actualStacks ).hasSize( 0 );
+	}
+
+	@Test
+	public void extractStacks_columnsNominatorWithNonDigits() {
+		List<Stack<Character>> actualStacks = supplyStacks.generateStacksFromContent(
+				"columnnominatorwithnondigits.txt" );
+		assertThat( actualStacks ).hasSize( 0 );
+	}
 }
