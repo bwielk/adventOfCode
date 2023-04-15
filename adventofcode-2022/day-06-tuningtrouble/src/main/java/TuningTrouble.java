45d59f0206cf792049b362467186977dae2ae03a
Blaise Wielk <bwielk@gmail.com>
2023-04-15 19:00:11 +0100

    day 6 challenge part 2 resolved - introduced a marker interval

--- adventofcode-2022/day-06-tuningtrouble/src/main/java/TuningTrouble.java
+++ adventofcode-2022/day-06-tuningtrouble/src/main/java/TuningTrouble.java
@@ -3,27 +3,25 @@ import java.util.List;
 
 public class TuningTrouble {
 
-	public static final int INTERVAL = 4;
-
-	public List<Integer> findSignalIndexFromFile( String fileName ) {
+	public List<Integer> findSignalIndexFromFile( String fileName, int markerInterval ) {
 		List<Integer> results = new ArrayList<>();
 		List<String> input = FileReaderHelper.readFileAsLinesOfStrings( TuningTrouble.class,
 				fileName );
 		for ( String in : input ) {
-			results.add( findSignalIndex( in ) );
+			results.add( findSignalIndex( in, markerInterval ) );
 		}
 		return results;
 	}
 
-	public int findSignalIndex( final String input ) {
+	public int findSignalIndex( final String input, int markerInterval ) {
 		int indexFound = -1;
 		String steriliseInput = input.trim().replace( " ", "" );
-		if ( steriliseInput.length() >= INTERVAL ) {
+		if ( steriliseInput.length() >= markerInterval ) {
 			for ( int i = 0; i < steriliseInput.length(); i++ ) {
-				if ( i + INTERVAL <= steriliseInput.length() ) {
+				if ( i + markerInterval <= steriliseInput.length() ) {
 					if ( StringHelper.isStringMadeOfUniqueChars(
-							steriliseInput.substring( i, i + INTERVAL ) ) ) {
-						indexFound = i + INTERVAL;
+							steriliseInput.substring( i, i + markerInterval ) ) ) {
+						indexFound = i + markerInterval;
 						break;
 					}
 				}
--- adventofcode-2022/day-06-tuningtrouble/src/test/java/TuningTroubleTest.java
+++ adventofcode-2022/day-06-tuningtrouble/src/test/java/TuningTroubleTest.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 class TuningTroubleTest {
 
 	private TuningTrouble tuningTrouble;
+	public static final int PACKET_INTERVAL = 4;
+	public static final int MESSAGE_INTERVAL = 14;
 
 	@BeforeEach
 	public void before() {
@@ -19,20 +21,27 @@ class TuningTroubleTest {
 	}
 
 	private static Stream<Arguments> findIndexOfAMarker() {
-		return Stream.of( Arguments.of( "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7 ),
-				Arguments.of( "bvwbjplbgvbhsrlpgdmjqwftvncz", 5 ), Arguments.of( "bvwk", 4 ),
-				Arguments.of( "1234", 4 ),// numeric characters only
-				Arguments.of( "a2bc", 4 ),// mixture of alphabetic and numeric characters
-				Arguments.of( "a!.[", 4 ),// mixture of alphabetic and non-alphabetic characters
-				Arguments.of( "nppdvjthqldpwncqszvftbrmjlhg", 6 ),
-				Arguments.of( "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10 ),
-				Arguments.of( "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11 ) );
+		return Stream.of(
+				Arguments.of( "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, PACKET_INTERVAL ),
+				Arguments.of( "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19, MESSAGE_INTERVAL ),
+				Arguments.of( "bvwbjplbgvbhsrlpgdmjqwftvncz", 5, PACKET_INTERVAL ),
+				Arguments.of( "bvwbjplbgvbhsrlpgdmjqwftvncz", 23, MESSAGE_INTERVAL ),
+				Arguments.of( "nppdvjthqldpwncqszvftbrmjlhg", 6, PACKET_INTERVAL ),
+				Arguments.of( "nppdvjthqldpwncqszvftbrmjlhg", 23, MESSAGE_INTERVAL ),
+				Arguments.of( "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, PACKET_INTERVAL ),
+				Arguments.of( "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29, MESSAGE_INTERVAL ),
+				Arguments.of( "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, PACKET_INTERVAL ),
+				Arguments.of( "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26, MESSAGE_INTERVAL ),
+				Arguments.of( "bvwk", 4, PACKET_INTERVAL ),
+				Arguments.of( "1234", 4, PACKET_INTERVAL ),// numeric characters only
+				Arguments.of( "a2bc", 4, PACKET_INTERVAL ),// mixture of alphabetic and numeric characters
+				Arguments.of( "a!.[", 4, PACKET_INTERVAL));// mixture of alphabetic and non-alphabetic characters
 	}
 
 	@ParameterizedTest
 	@MethodSource("findIndexOfAMarker")
-	public void markerIndexCanBeFound_regularInputs( String input, int expectedResult ) {
-		int result = tuningTrouble.findSignalIndex( input );
+	public void markerIndexCanBeFound_regularInputs( String input,  int expectedResult, int markerInterval) {
+		int result = tuningTrouble.findSignalIndex( input, markerInterval );
 		assertThat( result ).isEqualTo( expectedResult );
 	}
 
@@ -43,25 +52,25 @@ class TuningTroubleTest {
 	@ParameterizedTest
 	@MethodSource("unacceptableInputLengths")
 	public void inputLengthMustBeGreaterOrEqualThanIntervalOf4( String input ) {
-		int result = tuningTrouble.findSignalIndex( input );
+		int result = tuningTrouble.findSignalIndex( input, PACKET_INTERVAL );
 		assertThat( result ).isEqualTo( -1 );
 	}
 
 	@Test
 	public void markerIndexCanBeFound_duplicatedSetsOfUniqueCharacter() {
-		int result = tuningTrouble.findSignalIndex( "linklink" );
+		int result = tuningTrouble.findSignalIndex( "linklink", PACKET_INTERVAL );
 		assertThat( result ).isEqualTo( 4 );
 	}
 
 	@Test
 	public void markerIndexCanBeFound_spacesAreDroppedAndNotTreatedAsUniqueCharacter() {
-		int result = tuningTrouble.findSignalIndex( "mna mna " );
+		int result = tuningTrouble.findSignalIndex( "mna mna ", PACKET_INTERVAL );
 		assertThat( result ).isEqualTo( -1 );
 	}
 
 	@Test
 	public void markerIndexCanBeFound_noCaseSensitivity() {
-		int result = tuningTrouble.findSignalIndex( "mnaM" );
+		int result = tuningTrouble.findSignalIndex( "mnaM", PACKET_INTERVAL );
 		assertThat( result ).isEqualTo( -1 );
 	}
 
@@ -73,7 +82,7 @@ class TuningTroubleTest {
 	@MethodSource("trailingAndLeadingSpaces")
 	public void markerIndexCanBeFound_trailingAndLeadingSpacesAreDropped( String input,
 			int expectedResult ) {
-		int result = tuningTrouble.findSignalIndex( input );
+		int result = tuningTrouble.findSignalIndex( input, PACKET_INTERVAL );
 		assertThat( result ).isEqualTo( expectedResult );
 	}
 
@@ -81,13 +90,20 @@ class TuningTroubleTest {
 	public void filesCanBeProcessedAndFindSignalIndexes() {
 		List<Integer> expected = List.of( -1, 4, -1, 7 );
 		assertThat( tuningTrouble.findSignalIndexFromFile(
-				"test_multipleLines.txt" ) ).containsExactlyElementsOf( expected );
+				"test_multipleLines.txt", PACKET_INTERVAL ) ).containsExactlyElementsOf( expected );
 	}
 
 	@Test
-	public void masterFileIsProcessedCorrectly() {
-		List<Integer> result = tuningTrouble.findSignalIndexFromFile( "input.txt" );
+	public void masterFileIsProcessedCorrectly_packetInterval() {
+		List<Integer> result = tuningTrouble.findSignalIndexFromFile( "input.txt", PACKET_INTERVAL );
 		assertThat( result ).hasSize( 1 );
 		assertThat( result.get( 0 ) ).isEqualTo( 1850 );
+	}
+
+	@Test
+	public void masterFileIsProcessedCorrectly_messageInterval() {
+		List<Integer> result = tuningTrouble.findSignalIndexFromFile( "input.txt", MESSAGE_INTERVAL );
+		assertThat( result ).hasSize( 1 );
+		assertThat( result.get( 0 ) ).isEqualTo( 2823 );
 	}
 }
