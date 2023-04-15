afe26373465a9a4fbfa62d0852e8498fe5f16712
Blaise Wielk <bwielk@gmail.com>
2023-04-15 19:53:53 +0100

    day 7 setup

---
+++ adventofcode-2022/day-07-nospaceleftonthedevice/src/main/java/FileSystemActions.java
@@ -0,0 +1,17 @@
+public enum FileSystemActions {
+
+	MOVE_TO_DIR("cd "),
+	MOVE_OUT_LEVEL_UP("cd .."),
+	MOVE_TO_OUTERMOST("cd /"),
+	LIST_FILES_AND_DIRS("ls");
+
+	String command;
+
+	FileSystemActions(String command){
+		this.command = command;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+}
